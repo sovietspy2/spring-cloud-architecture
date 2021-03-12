f5bc46680180532890b3ec074794cd4528eeda32
SovietSpy2 <sovietspy2@users.noreply.github.com>
2021-03-12 17:57:17 +0100

    added logging

--- warehouse-service/src/main/java/stream/wortex/cloud/warehouseservice/controller/ProductController.java
+++ warehouse-service/src/main/java/stream/wortex/cloud/warehouseservice/controller/ProductController.java
@@ -1,8 +1,9 @@
 package stream.wortex.cloud.warehouseservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import stream.wortex.cloud.warehouseservice.model.Product;
 
@@ -13,6 +14,8 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 public class ProductController {
+
+    private final Logger log = LoggerFactory.getLogger(ProductController.class.getName());
 
     private final List<Product> data = new ArrayList<>();
 
@@ -30,11 +33,15 @@ public class ProductController {
 
     @GetMapping("/product/{id}")
     public Product getProductById(@PathVariable Long id) {
+        log.info("/product/id accessed");
+        System.out.println("/product/id");
         return data.stream().filter(d->d.getId().equals(id)).findFirst().orElseThrow();
     }
 
     @GetMapping("/product")
     public Product getProduct() throws InterruptedException {
+        log.info(" /product accessed");
+        System.out.println("/product");
         TimeUnit.SECONDS.sleep(2);
         return  data.get(0);
     }
