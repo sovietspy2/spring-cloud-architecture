6fe619185a071a959ad45ce6fb310a927c996041
SovietSpy2 <sovietspy2@users.noreply.github.com>
2021-03-12 17:56:46 +0100

    added circuit breaker

--- loadbalancer/src/main/java/stream/wortex/cloud/loadbalancer/LoadbalancerApplication.java
+++ loadbalancer/src/main/java/stream/wortex/cloud/loadbalancer/LoadbalancerApplication.java
@@ -1,29 +1,23 @@
 package stream.wortex.cloud.loadbalancer;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@RefreshScope
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class LoadbalancerApplication {
 
-    @Value("${warehouse}")
-    private String warehouseServiceAddress;
-
-    @Value("${payment}")
-    private String paymentServiceAddress;
-
     public static void main(String[] args) {
         SpringApplication.run(LoadbalancerApplication.class, args);
     }
-
 
     @LoadBalanced
     @Bean
